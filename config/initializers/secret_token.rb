b0807fd80b27e5e03774b6ac6a64a6f58cda9687
Ben Rexin <salzig+github@gmail.com>
2012-10-01 18:57:03 +0200

    initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Delens::Application.config.secret_token = 'df5e1dbad68ea9a818c49abd29f34e0d3a9f3565ea7be70dc7f67d4b9f9660ad8175390e2654467a07c0b1302c628c2a77c04982c0c92006ac87936bfb9fb64f'
