ef3511bbca81d2d056cced1922537b5043c83103
Ben Rexin <salzig+github@gmail.com>
2012-10-02 15:00:14 +0200

    remove ugly nbsp

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Group do
-  it { should have_attribute :name }
+  it { should have_attribute :name }
   it { should belong_to :kindergarten }
   it { should have_and_belong_to_many :children }
 end
