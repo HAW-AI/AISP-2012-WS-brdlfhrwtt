27665c16a766ec092206fbb0902b80502f3e0897
Ben Rexin <salzig+github@gmail.com>
2012-10-28 22:28:34 +0100

    fixing specs after some other breaks it. Fucking Yeah

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -5,6 +5,8 @@ FactoryGirl.define do
     name { Faker::Product::brand }
     kindergarten
     limit { rand(1..40) }
+    starts_on { Time.parse(['6:00', '6:30', '7:00', '8:00'].sample) }
+    ends_on { starts_on + (Kernel.rand(5)+3).hours }
 
     ignore do child_count 1 end
     after(:create) do |group, evaluator|
