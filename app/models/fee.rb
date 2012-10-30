bd0527032300ada7fab62a8f8e0c0f128f201fb9
Ben Rexin <salzig+github@gmail.com>
2012-10-30 13:01:01 +0100

    editable fee

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -1,6 +1,8 @@
 class Fee < ActiveRecord::Base
   has_many :rates
 
+  accepts_nested_attributes_for :rates
+
   validates :care_time, numericality: { greater_than_or_equal_to: 1 }
   validates :max_age, numericality: { greater_than_or_equal_to: 1 }
 end
