3b8434b91c3a0f3d315111423eae48fe6a143850
Ben Rexin <salzig+github@gmail.com>
2012-10-31 17:33:39 +0100

    use random names

--- spec/factories/children.rb
+++ spec/factories/children.rb
@@ -2,8 +2,8 @@
 
 FactoryGirl.define do
   factory :child do
-    nick "Peter"
-    firstname "Peter"
-    lastname "Lustig"
+    nick { Faker::Name.first_name }
+    firstname { (0..rand(2)).inject(nick) {|a,e| "#{a} #{Faker::Name.first_name}" } }
+    lastname { Faker::Name.last_name }
   end
 end
