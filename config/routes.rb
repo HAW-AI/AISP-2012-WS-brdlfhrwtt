8b9208851a79f9060ffdc4acd17d2343db9e8d65
Ben Rexin <salzig+github@gmail.com>
2012-11-01 00:07:41 +0100

    add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Delens::Application.routes.draw do
+  resource :dashboard
+
   resources :kindergartens, shallow: true do
     resources :groups, shallow: true do
       resources :children
     end
   end
+  resources :children, only: [:new, :create, :show]
 
   resources :fees
-  root to: 'kindergartens#index'
+  root to: 'dashboards#show'
 end
