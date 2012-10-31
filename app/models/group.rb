f59d6ac93de2ff1ec8130d23a6d18399d5551a35
Ben Rexin <salzig+github@gmail.com>
2012-10-31 18:24:21 +0100

    fix group create

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :limit, :starts_on, :ends_on
 
   has_many :children_groups
   has_many :children, :through => :children_groups
