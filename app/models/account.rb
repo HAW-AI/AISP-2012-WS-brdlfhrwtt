4a485bab0baed91e862945d771ed3180f9b025c3
Till Theis <theis.till@gmail.com>
2012-11-01 01:02:14 +0100

    make it possible to create accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,5 @@
 class Account < ActiveRecord::Base
-  attr_accessible :birth_sequence_position, :child, :child_count, :day_care_coupon, :free_preschool_year, :net_income, :person_count
+  attr_accessible :birth_sequence_position, :child, :child_count, :day_care_coupon, :free_preschool_year, :net_income, :person_count, :child_id
 
   belongs_to :child
 
