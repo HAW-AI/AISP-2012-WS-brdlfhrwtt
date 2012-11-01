5d1d6d68a89826d280da6bc5067203a4093ec16c
Ben Rexin <salzig+github@gmail.com>
2012-11-01 01:27:52 +0100

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate
+
+  private
+  def authenticate
+    authenticate_or_request_with_http_basic("Delens!") do |username, password|
+      username == 'ci' && password == 'joe'
+    end
+  end
 end
