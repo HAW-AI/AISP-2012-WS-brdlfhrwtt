71e846ee33d6406c41715d9876ade6661de80009
Ben Rexin <salzig+github@gmail.com>
2012-11-01 09:20:25 +0100

    fixes

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,10 +2,32 @@ class AccountsController < ResourceController
   def show
     @account = Account.find(params[:id])
 
-    caring_time_seconds = @account.child.groups.map { |g| g.ends_on - g.starts_on }.sum
-    caring_time_hours = caring_time_seconds / 3600.0
+    seconds_per_day = @account.child.groups.map { |g| g.ends_on - g.starts_on }.sum
+    seconds_per_week = seconds_per_day * 5.days
+    @hours_per_week = seconds_per_week  / 1.hour
 
-    @money_to_pay = "Money for #{caring_time_hours} hours of caring"
+    fee = Fee.where(Fee.arel_table[:care_time].gt(@hours_per_week)).order(Fee.arel_table[:care_time].asc).first
+
+    unless fee
+      fee = Fee.order(Fee.arel_table[:care_time].asc).first
+    end
+
+    rate = fee.rates.where(Rate.arel_table[:net_income].gt(@account.net_income)).order(Rate.arel_table[:net_income]).first
+
+    @price = case @account.person_count
+    when 2
+      rate.two_people
+    when 3
+      rate.three_people
+    when 4
+      rate.four_people
+    when 5
+      rate.five_people
+    else
+      rate.six_people
+    end
+
+    @money_to_pay = @price
 
     super
   end
