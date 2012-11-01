18d4076a546531d2b8aa92dca773b13b02f0976c
Till Theis <theis.till@gmail.com>
2012-11-01 03:59:53 +0100

    seed rates and fees

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,69 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+
+def filename_to_path(filename)
+  File.dirname(__FILE__) + '/seeds/' + filename + '.csv'
+end
+
+def import_fees(filename)
+  row = CSV.open(filename_to_path(filename), 'rb', col_sep: ';').first.map!(&:strip)
+
+  max_age, care_time = row
+
+  fee = Fee.new
+  fee.max_age = max_age
+  fee.care_time = care_time
+
+  if fee.save then fee else nil end
+end
+
+def import_rates(filename)
+  # 1st fee
+  fee = import_fees("fee_" + filename)
+
+  # 2nd fee
+  if (File.exists?(filename_to_path("fee_" + filename + "2"))) then
+    fee2 = import_fees("fee_" + filename + "2")
+  end
+
+
+  # rate
+  CSV.open(filename_to_path(filename), 'rb', col_sep: ';').each do |row|
+    row.map!(&:strip)
+
+    net_income, two_people, three_people, four_people, five_people, six_people = row
+
+    rate = Rate.new
+    rate.net_income = net_income
+    rate.two_people = two_people
+    rate.three_people = three_people
+    rate.four_people = four_people
+    rate.five_people = five_people
+    rate.six_people = six_people
+
+    rate.fee = fee
+    rate.save
+
+    if fee2 then
+      rate = Rate.new
+      rate.net_income = net_income
+      rate.two_people = two_people
+      rate.three_people = three_people
+      rate.four_people = four_people
+      rate.five_people = five_people
+      rate.six_people = six_people
+
+      rate.fee = fee2
+      rate.save
+    end
+  end
+end
+
+('a'..'h').each do |letter|
+  filename = 'tabelle_' + letter
+  import_rates(filename)
+end
