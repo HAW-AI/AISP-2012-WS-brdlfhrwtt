588c9a48801fe1ca251ccc7ae8adea67bf086bf3
Till Theis <theis.till@gmail.com>
2012-11-01 02:35:14 +0100

    stub for calculating the money to pay

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,12 @@
 class AccountsController < ResourceController
+  def show
+    @account = Account.find(params[:id])
+
+    caring_time_seconds = @account.child.groups.map { |g| g.ends_on - g.starts_on }.sum
+    caring_time_hours = caring_time_seconds / 3600.0
+
+    @money_to_pay = "Money for #{caring_time_hours} hours of caring"
+
+    super
+  end
 end
