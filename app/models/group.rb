929915dc53015840772a4829f58e4566a3d6f04f
Ben Rexin <salzig+github@gmail.com>
2012-11-01 03:10:14 +0100

    make waiting and children list ediable

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,13 +1,14 @@
 class Group < ActiveRecord::Base
-  attr_accessible :name, :limit, :starts_on, :ends_on, :waitlists_attributes
+  attr_accessible :name, :limit, :starts_on, :ends_on, :child_ids, :waiting_child_ids, :waitlists_attributes, :children_groups_attributes
 
-  has_many :children_groups
+  has_many :children_groups, include: :child
   has_many :children, :through => :children_groups
   has_many :waitlists, :include => :child
   has_many :waiting_children, :through => :waitlists, :source => :child
   belongs_to :kindergarten
 
   accepts_nested_attributes_for :waitlists
+  accepts_nested_attributes_for :children_groups
 
   validates :name, presence: true
   validates :limit, numericality: { greater_than_or_equal_to: 1 }
