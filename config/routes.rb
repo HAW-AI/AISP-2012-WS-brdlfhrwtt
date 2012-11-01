a87d33478d8df200a5eebebaf28fa4f431627afe
Ben Rexin <salzig+github@gmail.com>
2012-11-01 01:20:04 +0100

    waitlist edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Delens::Application.routes.draw do
+  root to: 'dashboards#show'
+
   resource :dashboard
+  resources :fees
   resources :accounts
   resources :children, only: [:new, :create, :show]
 
   resources :kindergartens, shallow: true do
     resources :groups, shallow: true do
       resources :children
+      member do
+        get :waitlist
+      end
     end
   end
 
-  resources :fees
-  root to: 'dashboards#show'
 end
