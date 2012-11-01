053a790f864bfba1e2060886fea152f3f818920f
Ben Rexin <salzig+github@gmail.com>
2012-11-01 05:19:28 +0100

    add wishlist to child new

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,6 +4,7 @@ class Child < ActiveRecord::Base
   has_many :children_groups
   has_many :groups, :through => :children_groups
   has_many :waitlists, include: :group
+  has_many :waiting_groups, :through => :waitlists, :source => :group
   belongs_to :parent
 
   validates :nick, presence: true
