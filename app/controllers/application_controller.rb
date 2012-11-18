9f21c8bb6fd551b197c17020a61221df59918043
Ben Rexin <salzig+github@gmail.com>
2012-11-18 16:29:33 +0100

    add runtime information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :runtime
 
+private
+  def runtime
+    response.headers['X-Powered-By'] = "HAW-Hamburg, ClubMate, Strawberries, Love"
+    response.headers['X-Version'] = "SuperKiGaV delens (en: destructive)"
+  end
 end
