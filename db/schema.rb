c21973d8044ee200dc474fa1ec7d1eb35ad6e711
Ben Rexin <salzig+github@gmail.com>
2012-11-21 18:49:28 +0100

    ups, missed the schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121101005606) do
+ActiveRecord::Schema.define(:version => 20121121162824) do
 
   create_table "accounts", :force => true do |t|
     t.boolean  "day_care_coupon"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(:version => 20121101005606) do
     t.boolean  "free_preschool_year"
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
+  end
+
+  create_table "authentications", :force => true do |t|
+    t.integer  "user_id",    :null => false
+    t.string   "provider",   :null => false
+    t.string   "uid",        :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "children", :force => true do |t|
@@ -90,6 +98,15 @@ ActiveRecord::Schema.define(:version => 20121101005606) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.integer  "fee_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username",         :null => false
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
   create_table "waitlists", :force => true do |t|
