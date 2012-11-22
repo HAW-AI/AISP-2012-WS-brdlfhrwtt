af5b22c8836fd83814d9e4be472fb818d09ac755
Ben Rexin <salzig+github@gmail.com>
2012-11-22 08:22:22 +0100

    add states and catch if not present

---
+++ db/seeds/states.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+def load_states
+State.create(name: "Baden-Württemberg")
+State.create(name: "Bayern")
+State.create(name: "Berlin ")
+State.create(name: "Brandenburg")
+State.create(name: "Bremen ")
+State.create(name: "Hamburg ")
+State.create(name: "Hessen")
+State.create(name: "Mecklenburg-Vorpommern")
+State.create(name: "Niedersachsen")
+State.create(name: "Nordrhein-Westfalen")
+State.create(name: "Rheinland-Pfalz")
+State.create(name: "Saarland")
+State.create(name: "Sachsen")
+State.create(name: "Sachsen-Anhalt")
+State.create(name: "Schleswig-Holstein")
+State.create(name: "Thüringen")
+end
