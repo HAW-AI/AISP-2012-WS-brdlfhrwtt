13cd1fe173391cabe88fbc74246b88e9d8092a2e
Till Theis <theis.till@gmail.com>
2012-11-22 02:07:13 +0100

    state model for german federal states

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121121223050) do
+ActiveRecord::Schema.define(:version => 20121122010226) do
 
   create_table "accounts", :force => true do |t|
     t.boolean  "day_care_coupon"
@@ -105,6 +105,12 @@ ActiveRecord::Schema.define(:version => 20121121223050) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.integer  "fee_id"
+  end
+
+  create_table "states", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
---
+++ spec/models/state_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe State do
+  it { should have_attribute(:name) }
+
+  it { should allow_value("Hamburg").for(:name) }
+  it { should_not allow_value("").for(:name) }
+  it { should_not allow_value(nil).for(:name) }
+end
