3903069e67cf48ef74f237a5708bd6b6324aaf36
Ben Rexin <salzig+github@gmail.com>
2012-11-22 03:14:00 +0100

    use page_after_login helper, again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     respond_to do |format|
       if @user = login(params[:email],params[:password])
-        format.html { redirect_back_or_to(root_url(subdomain:'parent'), notice: 'Login successfull.') }
+        format.html { redirect_back_or_to(page_after_login, notice: 'Login successfull.') }
         format.xml { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { flash.now[:alert] = 'Login failed.'; render :action => "new" }
