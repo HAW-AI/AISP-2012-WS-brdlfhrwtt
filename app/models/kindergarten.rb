25dfcab543d22137f919d7063f06b0e76df49503
Ben Rexin <salzig+github@gmail.com>
2012-11-22 03:19:32 +0100

    count via current_user

--- app/models/kindergarten.rb
+++ app/models/kindergarten.rb
@@ -1,6 +1,7 @@
 class Kindergarten < ActiveRecord::Base
   attr_accessible :name
   has_many :groups
+  has_many :children, :through => :groups
   has_one :manager
 
   validates :name, presence: true
