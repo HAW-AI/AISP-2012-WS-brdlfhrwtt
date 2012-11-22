01c326845d0baf63f78e6c3ebec54fd67644b276
Ben Rexin <salzig+github@gmail.com>
2012-11-22 02:42:11 +0100

    inject @kindergarten in association_chain for ParentsController

--- app/controllers/manager/parents_controller.rb
+++ app/controllers/manager/parents_controller.rb
@@ -1,8 +1,12 @@
 class Manager::ParentsController < Manager::ResourceController
-  belongs_to :kindergarten, shallow: true
   custom_actions :resource => :waitlist
 
   def index
     @parents = Parent.scoped
+  end
+
+protected
+  def begin_of_association_chain
+    @kindergarten
   end
 end
