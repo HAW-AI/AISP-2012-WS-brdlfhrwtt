8166617455d6de3aebdcf72664cff4e35e0c3d4f
Ben Rexin <salzig+github@gmail.com>
2012-11-22 09:02:38 +0100

    fix what ever

--- app/controllers/manager/accounts_controller.rb
+++ app/controllers/manager/accounts_controller.rb
@@ -8,15 +8,16 @@ class Manager::AccountsController < Manager::ResourceController
 
   def show
     @account = Account.find(params[:id])
+    @state = @account.child.groups.map{|g| g.kindergarten}.map{|k| k.state}.uniq.first
 
     seconds_per_day = @account.child.groups.map { |g| g.ends_on - g.starts_on }.sum
     seconds_per_week = seconds_per_day * 5.days
     @hours_per_week = seconds_per_week  / 1.hour
 
-    fee = Fee.where(Fee.arel_table[:care_time].gt(@hours_per_week)).order(Fee.arel_table[:care_time].asc).first
+    fee = @state.fees.where(Fee.arel_table[:care_time].gt(@hours_per_week)).order(Fee.arel_table[:care_time].asc).first
 
     unless fee
-      fee = Fee.order(Fee.arel_table[:care_time].asc).first
+      fee = @state.fees.order(Fee.arel_table[:care_time].asc).first
     end
 
     rate = fee.rates.where(Rate.arel_table[:net_income].gt(@account.net_income)).order(Rate.arel_table[:net_income]).first
